fc24357cb8c1a0079a69ec9860a64dd6c2ce49e8
Theo Lee <theo.e.lee@gmail.com>
2021-04-21 19:42:35 -0700

    set master userName and password

--- server.js
+++ server.js
@@ -6,15 +6,27 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.raw());
-
 app.use(cors());
+
+const mainUserName = "helloWorld@test.com";
+const mainPassword = "helloWorld";
 
 app.use('/login', (req, res) => {
     console.log(`${req.body.username} - ${req.body.password}`);
-    res.send({
-        userName: req.body.userName,
-        password: req.body.password
-    })
+
+    if (req.body.username === mainUserName && req.body.password === mainPassword) {
+        res.send(true);
+        // res.send({
+        //     userName: req.body.userName,
+        //     password: req.body.password
+        // })
+    } else {
+        // res.send(
+        //     userName: null,
+        //     password: null
+        // })
+        res.send(false);
+    }
 });
 
 app.listen(8080, () => console.log('API is running on http://localhost:8080/login'));
