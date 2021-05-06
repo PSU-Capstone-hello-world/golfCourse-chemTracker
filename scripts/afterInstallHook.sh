57ec68a70023c6352a5a232ec6090a04f81ab4b4
danielchafner <42757995+danielchafner@users.noreply.github.com>
2021-05-05 20:37:42 -0700

    changing permissions

--- scripts/afterInstallHook.sh
+++ scripts/afterInstallHook.sh
@@ -3,6 +3,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 set -e
 cd /home/ec2-user
+sudo chown -R $(whoami) ~/.npm
 npm install
 npm cache clean
 npm run build
