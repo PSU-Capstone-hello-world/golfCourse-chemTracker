35875ad07414842433e28897e2da3f6d27723366
daemon29 <34591979+daemon29@users.noreply.github.com>
2021-05-05 17:59:41 -0700

    add getDocFromImage, and modify location part of the document

--- src/model/backend.js
+++ src/model/backend.js
@@ -99,9 +99,6 @@ class Backend{
         const response = await axios.post("https://c7fjg6xclk.execute-api.us-west-2.amazonaws.com/beta/", body)
         return JSON.parse(response.data['body'])
     }
-    async getDocFromImage(image) {
-        
-    }
     async getItem() {
         
     }
@@ -119,6 +116,98 @@ class Backend{
             return JSON.parse(response.data['body'])
         }
     }
+    async getDocFromImage(image) {
+        try {
+            let image_base64 = await this.convertBase64(image)
+            const body = {
+                'Image':  image_base64.replace(/^data:image\/\w+;base64,/, '') 
+            }
+            const response = await axios.post("https://c7fjg6xclk.execute-api.us-west-2.amazonaws.com/beta/gettextfromimage", body);
+            let temp  ={}
+            let result = String(response.data['body']).split(/\\n/)
+            for (var i = 0; i < result.length; i++){
+                if (result[i] === "Product Name:")
+                    temp['productName'] = result[i + 1]
+                if (result[i] === "Supplier:")
+                    temp['supplier'] = result[i + 1]
+                if (result[i] === "EPA Registration #")
+                    temp['epaRegNum'] = result[i + 1]
+                if (result[i] === "EPA Est. #")
+                    temp['epaEstNum'] = result[i + 1]
+                if (result[i] === "Target:")
+                    temp['target'] = result[i + 1]
+                if (result[i] === "Vehicle:")
+                    temp['vehicle'] = result[i + 1]
+                if (result[i] === "Gear:")
+                    temp['gear'] = result[i + 1]
+                if (result[i] === "RPM:")
+                    temp['rpm'] = result[i + 1]
+                if (result[i] === "MPH:")
+                    temp['mph'] = result[i + 1]
+                if (result[i] === "Sprayer/Spreader:")
+                    temp['sprayer'] = result[i + 1]
+                if (result[i] === "Nozzles/Setting:")
+                    temp['nozzle'] = result[i + 1]
+                if (result[i] === "Pressure:")
+                    temp['pressure'] = result[i + 1]
+                if (result[i] === "Tank Mix: Amount of Product:")
+                    temp['tankAmt'] = result[i + 1]
+                if (result[i] === "`per")
+                    temp['tankWater'] = result[i + 1]
+                if (result[i] === "Adjuvant/Dye:")
+                    temp['adjuvant'] = result[i + 1]
+                if (result[i] === "Total Amount of Product Applied:")
+                    temp['totalApplied'] = result[i + 1]
+                if (result[i] === "Application rate:")
+                    temp['appRateOz'] = result[i + 1]
+                if (result[i] === "Minutes:")
+                    temp['wateredMin'] = result[i + 1]
+                if (result[i] === "Temperature:")
+                    temp['temp'] = result[i + 1]
+                if (result[i] === "F Humidity:")
+                    temp['humidity'] = result[i + 1]
+                if (result[i] === "% Wind:")
+                    temp['wind'] = result[i + 1]
+                if (result[i] === "Date of Application:")
+                    temp['date'] = result[i + 1]
+                if (result[i] === "PURS")
+                    temp['purs'] = result[i + 1]
+                if (result[i] === "Time of Application:")
+                    temp['timeStart'] = result[i + 1]
+                if (result[i] === "to")
+                    temp['timeEnd'] = result[i + 1]
+                if (result[i] === "How was container disposed:")
+                    temp['disposed'] = result[i + 1]
+                if (result[i] === "How was equipment cleaned:")
+                    temp['cleaned'] = result[i + 1]
+                if (result[i] === "Actual lbs of N applied per 1000 ft. 2")
+                    temp['lbsN'] = result[i - 1]
+                if (result[i] === "Actual lbs of P2 O 5 applied per 1000 ft. 2")
+                    temp['lbsP2O5'] = result[i - 1]
+                if (result[i] === "Actual lbs of K2 0 applied per 1000 ft. 2")
+                    temp['lbsK2O'] = result[i - 1]
+                if (result[i] === "Signature:")
+                    temp['signature'] = result[i + 1]
+                if (result[i] === "Date:")
+                    temp['sigDate'] = result[i + 1]
+            }
+            return Document(temp);
+        } catch (error) {
+            console.log("Error")
+        }
+    }
+    convertBase64 = (image) => {
+        return new Promise((resolve, reject) => {
+          const fileReader = new FileReader();
+          fileReader.readAsDataURL(image)
+          fileReader.onload = () => {
+            resolve(fileReader.result);
+          }
+          fileReader.onerror = (error) => {
+            reject(error);
+          }
+        })
+      }
 }
 
 export default Backend;
